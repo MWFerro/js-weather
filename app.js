d18dd1a5370107f4c28277e26eaf60f5442e0382
mwf <michael@mferro.com>
2023-11-02 12:45:39 +0000

    chartCheck

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const apiKey = 'fff54ec72d8341459e1213730230111';
 
 const temperatureData = [];
 
+let myChart;
+
 function updateTable() {
   const tableBody = document.getElementById('temperature-table');
   tableBody.innerHTML = '';
@@ -21,44 +23,54 @@ function updateChart() {
 
   const temperatures = temperatureData.slice(-100).map((data) => data.temperature);
 
-  const ctx = temperatureChart.getContext('2d');
-  new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: Array.from({ length: temperatures.length }, (_, i) => i),
-      datasets: [{
-        label: 'Temperature (°C)',
-        data: temperatures,
-        borderColor: 'blue',
-        borderWidth: 2,
-      }],
-    },
-  });
+  if (!myChart) {
+    const ctx = temperatureChart.getContext('2d');
+    myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: Array.from({ length: temperatures.length }, (_, i) => i),
+        datasets: [{
+          label: 'Temperature (°C)',
+          data: temperatures,
+          borderColor: 'blue',
+          borderWidth: 2,
+        }],
+      },
+    });
+  } else {
+    myChart.data.labels = Array.from({ length: temperatures.length }, (_, i) => i);
+    myChart.data.datasets[0].data = temperatures;
+    myChart.update();
+  }
 }
 
 function fetchWeatherData() {
-  const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=London`; 
+  const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=London`;
   fetch(apiUrl)
-    .then((response) => response.json())
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`API request failed with status: ${response.status}`);
+      }
+      return response.json();
+    })
     .then((data) => {
       const temperature = data.current.temp_c;
-      const dateTime = new Date(data.location.localtime);
+      const dateTime = new Date();
 
       temperatureData.push({ temperature, dateTime });
 
       if (temperatureData.length > 100) {
         temperatureData.shift();
       }
+
       updateTable();
-
       updateChart();
-
       document.getElementById('current-temperature').textContent = `${temperature}°C`;
-
-      setTimeout(fetchWeatherData, 3000);
+      setTimeout(fetchWeatherData, 3000); 
     })
     .catch((error) => {
       console.error('Error fetching weather data:', error);
+      setTimeout(fetchWeatherData, 3000);
     });
 }
 
